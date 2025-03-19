29a51f4ec83a3d593f0c37c87acdfbc002cb0357
JH <ryujh030820@gmail.com>
2025-03-19 14:14:01 +0900

    [fix] 거래 내역 페이지 수정

--- src/app/(afterLogin)/transactions/_component/DeleteTransactionsModal.tsx
+++ src/app/(afterLogin)/transactions/_component/DeleteTransactionsModal.tsx
@@ -3,7 +3,6 @@
 import { useModalStore } from '@/app/store/modal';
 import axiosInstance from '@/utils/axiosInstance';
 import Swal from 'sweetalert2';
-import { Trash2 } from 'lucide-react';
 
 interface DeleteTransactionsModalProps {
   selectedItems: number[];
@@ -46,35 +45,34 @@ export default function DeleteTransactionsModal({
 
   return (
     <>
-      <div className="fixed inset-0 bg-black/25 z-[60]"></div>
-      <div className="fixed top-[12%] left-1/2 z-[70] w-full max-w-lg -translate-x-1/2 -translate-y-1/2">
-        <div className="bg-white rounded-lg shadow-xl p-6 animate-fade-in-down">
-          <div className="pb-4 mb-4 border-b">
-            <div className="flex flex-row justify-between items-center">
-              <h2 className="text-xl font-semibold">거래 내역 삭제</h2>
+      <div className='fixed inset-0 bg-black/25 z-[60]'></div>
+      <div className='fixed top-[12%] left-1/2 z-[70] w-full max-w-lg -translate-x-1/2 -translate-y-1/2'>
+        <div className='bg-white rounded-lg shadow-xl p-6 animate-fade-in-down'>
+          <div className='pb-4 mb-4 border-b'>
+            <div className='flex flex-row justify-between items-center'>
+              <h2 className='text-xl font-semibold'>거래 내역 삭제</h2>
               <button
-                className="text-3xl text-slate-300 hover:text-[#3699ff] transition-all"
+                className='text-3xl text-slate-300 hover:text-[#3699ff] transition-all'
                 onClick={() => setIsTransactionsDeleteModalOpen(false)}
               >
                 ×
               </button>
             </div>
           </div>
-          <div className="pb-4 mb-4 text-sm text-slate-700">
+          <div className='pb-4 mb-4 text-sm text-slate-700'>
             정말 해당 거래 내역을 삭제하시겠습니까?
           </div>
-          <div className="flex flex-row justify-end gap-2">
+          <div className='flex flex-row justify-end gap-2'>
             <button
-              className="px-6 py-3 bg-slate-100 hover:bg-slate-200 text-sm font-semibold text-slate-500 rounded-md transition-all"
+              className='px-6 py-3 bg-slate-100 hover:bg-slate-200 text-sm font-semibold text-slate-500 rounded-md transition-all'
               onClick={() => setIsTransactionsDeleteModalOpen(false)}
             >
               취소
             </button>
             <button
-              className="px-6 py-3 bg-red-400 hover:bg-red-500 text-sm font-semibold text-white rounded-md transition-all"
+              className='px-6 py-3 bg-red-400 hover:bg-red-500 text-sm font-semibold text-white rounded-md transition-all'
               onClick={handleConfirmDelete}
             >
-              <Trash2 className="w-4 h-4 mr-1" />
               삭제
             </button>
           </div>
